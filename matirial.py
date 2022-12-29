1a03d75e050a6cd6cacda79202d8db1d057c7b6c
Sanjaya Danushka <62214882+Sanjaya-Danushka@users.noreply.github.com>
2022-12-29 15:07:28 +0530

    Add files via upload

---
+++ matirial.py
@@ -0,0 +1,21 @@
+import ctypes as ct
+from tkinter import *
+
+
+def dark_title_bar(window):
+    window.update()
+    DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+    set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+    get_parent = ct.windll.user32.GetParent
+    hwnd = get_parent(window.winfo_id())
+    rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
+    value = 2
+    value = ct.c_int(value)
+    set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
+
+
+root = Tk()
+# ---------------------
+dark_title_bar(root)
+# --------------------
+root.mainloop()
